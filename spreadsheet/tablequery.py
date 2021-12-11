191d97d9061ad1da4d34a8cd992fc6850754f2b8
Wolfgang Fahl <wf@bitplan.com>
2021-12-11 18:06:46 +0100

    refactors to improve #6 and #7

--- spreadsheet/tablequery.py
+++ spreadsheet/tablequery.py
@@ -42,6 +42,33 @@ class TableQuery(object):
         self.queries={}
         self.tableEditing=TableEditing()
         
+    def addQuery(self,query:Query):
+        '''
+        add the query with the given name to my queries
+        
+        query(Query): the query to add
+        '''
+        self.queries[query.name]=query
+        
+    def fetchQueryResults(self):
+        '''
+        fetch the QueryResults 
+        
+        '''
+        for _name,query in self.queries.items():
+            lod=None
+            if query.lang=="ask":
+                if not hasattr(query, "wikiAccess") or query.wikiAccess is None:
+                    raise(f"wikiAccess needs to be configured for Semantic MediaWiki ask query '{query.name}'")
+                lod=query.wikiAccess.smwClient.query(query.query)
+            elif query.lang.lower()=="sparql":
+                if not hasattr(query,"endpoint") or query.endpoint is None:
+                    raise(f"endpoint needs to be configured for SPARQL query '{query.name}'")
+                lod=query.endpoint.query(query.query)
+            if lod is not None:
+                self.tableEditing.addLoD(query.name, lod)
+        
+        
     def fromAskQueries(self,wikiId:str,askQueries:list):
         '''
         initialize me from the given Queries
@@ -55,7 +82,8 @@ class TableQuery(object):
             title=askQuery["title"] if "title" in askQuery else None
             description=askQuery["description"] if "description" in askQuery else None
             query=Query(name=name,query=ask,lang='ask',title=title,description=description,debug=self.debug)
-            self.queries[name]=query
-            lod=wikiAccess.smwClient.query(query.query)
-            self.tableEditing.addLoD(name, lod)
+            query.wikiAccess=wikiAccess
+            self.addQuery(query)
+        self.fetchQueryResults()
+            
             
--- tests/test_tableQuery.py
+++ tests/test_tableQuery.py
@@ -10,6 +10,9 @@ from spreadsheet.tableediting import SpreadSheetType
 from wikibot.wikiuser import WikiUser
 import os
 import getpass
+from lodstorage.query import Query
+from lodstorage.sparql import SPARQL
+
 
 class TestTableQuery(BaseTest):
     '''
@@ -19,49 +22,49 @@ class TestTableQuery(BaseTest):
     @classmethod
     def setUpClass(cls):
         super(TestTableQuery, cls).setUpClass()
-        cls.user=getpass.getuser()
+        cls.user = getpass.getuser()
 
     def setUp(self):
         BaseTest.setUp(self)
-        self.tq=None
+        self.tq = None
         pass
     
     @classmethod
-    def getSMW_WikiUser(cls,wikiId="smw"):
+    def getSMW_WikiUser(cls, wikiId="smw"):
         '''
         get semantic media wiki users for SemanticMediawiki.org and openresearch.org
         
         Args:
             wikiId(str): the wikiId to get the Semantic MediaWiki user for
         '''
-        iniFile=WikiUser.iniFilePath(wikiId)
-        wikiUser=None
+        iniFile = WikiUser.iniFilePath(wikiId)
+        wikiUser = None
         if not os.path.isfile(iniFile):
-            wikiDict=None
-            if wikiId=="smwcopy":
-                wikiDict={"wikiId": wikiId,"email":"webmaster@bitplan.com","url":"http://smw.bitplan.com","scriptPath":"/","version":"MediaWiki 1.35.0"}
-            if wikiId=="smw":
-                wikiDict={"wikiId": wikiId,"email":"webmaster@semantic-mediawiki.org","url":"https://www.semantic-mediawiki.org","scriptPath":"/w","version":"MediaWiki 1.31.16"}
-            if wikiId=="or":
-                wikiDict={"wikiId": wikiId,"email":"webmaster@openresearch.org","url":"https://www.openresearch.org","scriptPath":"/mediawiki/","version":"MediaWiki 1.31.1"}   
-            wikiDict["user"]=f"{cls.user}"
+            wikiDict = None
+            if wikiId == "smwcopy":
+                wikiDict = {"wikiId": wikiId, "email":"webmaster@bitplan.com", "url":"http://smw.bitplan.com", "scriptPath":"/", "version":"MediaWiki 1.35.0"}
+            if wikiId == "smw":
+                wikiDict = {"wikiId": wikiId, "email":"webmaster@semantic-mediawiki.org", "url":"https://www.semantic-mediawiki.org", "scriptPath":"/w", "version":"MediaWiki 1.31.16"}
+            if wikiId == "or":
+                wikiDict = {"wikiId": wikiId, "email":"webmaster@openresearch.org", "url":"https://www.openresearch.org", "scriptPath":"/mediawiki/", "version":"MediaWiki 1.31.1"}   
+            wikiDict["user"] = f"{cls.user}"
             if wikiDict is None:
                 raise Exception(f"{iniFile} missing for wikiId {wikiId}")
             else:
-                wikiUser=WikiUser.ofDict(wikiDict, lenient=True)
+                wikiUser = WikiUser.ofDict(wikiDict, lenient=True)
                 if BaseTest.inPublicCI():
                     wikiUser.save()
         else: 
-            wikiUser=WikiUser.ofWikiId(wikiId,lenient=True)
+            wikiUser = WikiUser.ofWikiId(wikiId, lenient=True)
         return wikiUser
     
     def getTableQuery(self):
         if self.tq is not None:
             return self.tq
-        wikiId="smw"
+        wikiId = "smw"
         # make sure wiki user is available
         TestTableQuery.getSMW_WikiUser(wikiId)
-        self.askQueries=[{
+        self.askQueries = [{
                 "name":"eventseries",
                 "ask":"[[SMWCon]]|mainlabel='pageTitle'"
             }, {
@@ -73,45 +76,84 @@ class TestTableQuery(BaseTest):
 |format=table
 }}"""
             }]
-        self.tq=TableQuery()
-        self.tq.fromAskQueries(wikiId=wikiId,askQueries=self.askQueries)
+        self.tq = TableQuery()
+        self.tq.fromAskQueries(wikiId=wikiId, askQueries=self.askQueries)
         return self.tq
 
     def testTableQuery(self):
         '''
         test table query handling
         '''
-        tq=self.getTableQuery()
-        self.assertEqual(2,len(tq.queries))
+        tq = self.getTableQuery()
+        self.assertEqual(2, len(tq.queries))
         self.assertFalse(tq.tableEditing is None)
-        lods=tq.tableEditing.lods
-        self.assertEqual(2,len(lods))
-        debug=self.debug
+        lods = tq.tableEditing.lods
+        self.assertEqual(2, len(lods))
+        debug = self.debug
         for askQuery in self.askQueries:
-            name=askQuery["name"]
+            name = askQuery["name"]
             self.assertTrue(name in lods)
-            lod=lods[name]
+            lod = lods[name]
             if debug:
                 print(len(lod))
                 print(lod)
-            if name=="events":
-                self.assertTrue(len(lod)>=18)
+            if name == "events":
+                self.assertTrue(len(lod) >= 18)
         
     def testSpreadSheetFromTableQuery(self):
         '''
         test creating a spreadSheet from a table Query
         '''
-        tq=self.getTableQuery()
-        name="testSMWCon"
-        s=tq.tableEditing.toSpreadSheet(spreadSheetType=SpreadSheetType.EXCEL,name=name)
+        tq = self.getTableQuery()
+        name = "testSMWCon"
+        s = tq.tableEditing.toSpreadSheet(spreadSheetType=SpreadSheetType.EXCEL, name=name)
         self.assertTrue(s is not None)
-        debug=self.debug
+        #debug = self.debug
+        debug=True
         if debug:
             print(type(s))
         # TODO add test
         pass
+    
+    def testSPARQLQuery(self):
+        '''
+        test SPARQL Query support
+        '''
+        testQueries = [
+        {
+            "endpoint":"https://query.wikidata.org/sparql",
+            "prefixes": ["http://www.wikidata.org/entity/"],
+            "lang": "sparql",
+            "name": "CityTop10",
+            "title": "Ten largest cities of the world",
+            "description": "Wikidata SPARQL query showing the 10 most populated cities of the world using the million city class Q1637706 for selection",
+            "query": """# Ten Largest cities of the world 
+# WF 2021-08-23
+# see also http://wiki.bitplan.com/index.php/PyLoDStorage#Examples
+# see also https://github.com/WolfgangFahl/pyLoDStorage/issues/46
+SELECT DISTINCT ?city ?cityLabel ?population ?country ?countryLabel 
+WHERE {
+  VALUES ?cityClass { wd:Q1637706}.
+  ?city wdt:P31 ?cityClass .
+  ?city wdt:P1082 ?population .
+  ?city wdt:P17 ?country .
+  SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "en" .
+  }
+}
+ORDER BY DESC(?population)
+LIMIT 10"""
+            }]
+        tq = TableQuery()
+        for queryMap in testQueries:
+            endpointUrl=queryMap.pop("endpoint")
+            _prefixes=queryMap.pop("prefixes")
+            query = Query(**queryMap)
+            query.endpoint=SPARQL(endpointUrl)
+            tq.addQuery(query)
+        tq.fetchQueryResults()
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
