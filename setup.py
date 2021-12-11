aaec0180709af4d0541b73e13ffdf126a053a448
Wolfgang Fahl <wf@bitplan.com>
2021-12-11 18:23:01 +0100

    prepares 0.0.2 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pyOnlineSpreadSheetEditing',
-    version='0.0.1',
+    version='0.0.2',
 
     packages=['spreadsheet',],
     author='Wolfgang Fahl',
