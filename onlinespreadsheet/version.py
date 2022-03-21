927278ab7b172013b2c29779b6e18a458c45c3b9
Wolfgang Fahl <wf@bitplan.com>
2022-03-21 08:01:00 +0100

    refactors Version to new file

---
+++ onlinespreadsheet/version.py
@@ -0,0 +1,14 @@
+'''
+Created on 2022-03-06
+
+@author: wf
+'''
+
+class Version(object):
+    '''
+    Version handling for pyOnlineSpreadsheetEditing
+    '''
+    name="pyOnlineSpreadsheetEditing"
+    version='0.1.27'
+    date = '2020-09-10'
+    updated = '2022-03-20'
