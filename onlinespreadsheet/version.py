03234afb0f70995ab92584a9f969aa7e961945ee
Wolfgang Fahl <wf@bitplan.com>
2022-05-15 15:29:26 +0200

    adds dependency and prepares release

--- onlinespreadsheet/version.py
+++ onlinespreadsheet/version.py
@@ -9,7 +9,7 @@ class Version(object):
     Version handling for pyOnlineSpreadsheetEditing
     '''
     name="pyOnlineSpreadsheetEditing"
-    version='0.0.8'
+    version='0.0.9'
     date = '2021-12-11'
-    updated = '2022-05-09'
+    updated = '2022-05-15'
     description='python Online SpreadSheet Editing tool with configurable enhancer/importer and check phase'
