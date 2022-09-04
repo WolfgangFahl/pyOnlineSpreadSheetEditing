0ef0fb9377b606d8b2cdfa2c0f05f733dfed6b05
Wolfgang Fahl <wf@bitplan.com>
2022-09-04 21:42:58 +0200

    use JpRoute

--- wd/wdbrowser.py
+++ wd/wdbrowser.py
@@ -249,9 +249,9 @@ class WikiDataBrowser(App):
             self.paretoLevels[level]=pareto
         self.ttTable=None
         # Routes
-        jp.Route('/settings',self.settings)
+        jp.JpRoute('/settings',self.settings)
         #jp.Route('/itemsearch',self.itemsearch)
-        jp.Route('/tt/{qid}',self.ttcontent)
+        jp.JpRoute('/tt/{qid}',self.ttcontent)
         self.starttime=time.time()
         self.wdProperty=WikidataProperty("P31")
 
