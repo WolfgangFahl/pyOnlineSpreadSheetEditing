71c2a5754fc0bb69da52e4ae0e870cedea8e90d3
Wolfgang Fahl <wf@bitplan.com>
2022-11-04 11:47:57 +0100

    prepares upgrade

--- onlinespreadsheet/version.py
+++ onlinespreadsheet/version.py
@@ -9,7 +9,7 @@ class Version(object):
     Version handling for pyOnlineSpreadsheetEditing
     '''
     name="pyOnlineSpreadsheetEditing"
-    version='0.0.15'
+    version='0.0.16'
     date = '2021-12-11'
     updated = '2022-11-04'
     description='python Online SpreadSheet Editing tool with configurable enhancer/importer and check phase'
