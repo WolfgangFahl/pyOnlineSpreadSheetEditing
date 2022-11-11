6096d8c75ff48b7cdb2a86db9c0b2faff9235511
Wolfgang Fahl <wf@bitplan.com>
2022-11-11 16:03:18 +0100

    upgrades py3rdpartymediawiki

--- onlinespreadsheet/version.py
+++ onlinespreadsheet/version.py
@@ -9,9 +9,9 @@ class Version(object):
     Version handling for pyOnlineSpreadsheetEditing
     '''
     name="pyOnlineSpreadsheetEditing"
-    version='0.0.16'
+    version='0.0.17'
     date = '2021-12-11'
-    updated = '2022-11-04'
+    updated = '2022-11-11'
     description='python Online SpreadSheet Editing tool with configurable enhancer/importer and check phase'
     authors='Wolfgang Fahl/Tim Holzheim'
     license=f'''Copyright 2022 contributors. All rights reserved.
